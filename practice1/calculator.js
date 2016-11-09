f8dd61388180a5a88c7f24f350fa49c165064340
rilakryan <onestepcloser03@gmail.com>
2016-11-10 01:10:39 +0800

    2

--- practice1/calculator.js
+++ practice1/calculator.js
@@ -6,27 +6,35 @@
   var answer = 0;
 
 function number(numbtn) {
-  if(input){
+  if(input){//若為true 則第一個數字進來  ex:9
     document.getElementById("result").value = numbtn;
-  } else{
+  } else{// false 9.9
     document.getElementById("result").value = document.getElementById("result").value + numbtn;
   }
-  input = false;
+  input = false;//若有第一個數字就改為false
 }
 
  function dot() {
     temp=document.getElementById("result").value
-      if (temp.indexOf('.')==-1){
-        document.getElementById("result").value=document.getElementById("result").value+".";
+    if (temp.indexOf('.')==-1){ //若沒有 . 則可以輸入 .
+      document.getElementById("result").value=document.getElementById("result").value+".";
     }
   }
-
-function opButton(opbtn) { 
+/**************
+*運算輸入
+* 1.若第一次輸入運算，且運算輸入的不是等號，把目前顯示的值為num1 ex 9 與 ans = 0  做一次相加，得到ans，輸入的opbtn運算符號交給operator準備做運算，input變為true可輸入數字。
+* 2.因為answer有值，所以使用operator做運算
+* ------
+* 若第一次輸入為等號則強制使用預設operator做加法得到ans的值。
+* --------
+* 若輸入等號則使用上次輸入的運算符號 opbtn-->operator-->equalop  做運算。算完清空ans 和改變input。
+* ********************/
+function opButton(opbtn) {
    if(input === false){
       num1 =  document.getElementById("result").value;
       answer = answer +'';
-      if((opbtn != '=') ){
-        if(answer == ''){
+      if((opbtn != '=') ) {
+        if(answer == '') {
           answer =eval('answer+num1');
           document.getElementById("result").value = answer;
         } else {
