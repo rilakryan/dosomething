e534f399911e9974432b3a0fb4d26bb74e423704
rilakryan <onestepcloser03@gmail.com>
2016-11-12 21:28:22 +0800

    site

---
+++ practice/site.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("load", function(event){
+    setInterval("repeatcover()",3000);
+});
+
+var cover = new Array("001.jpg","002.jpg","003.jpg");
+var covernum = cover.length;
+var i=1;
+function repeatcover(){
+    document.getElementById("cover").style.backgroundImage = "url(img/"+cover[i]+")";
+    i++
+    if(i>=covernum)  i=0;
+}
